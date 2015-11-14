d823edf3252a6fa5c308b2966be7ae11030260e6
neverlock <neverlock@gmail.com>
2015-11-14 15:16:26 +0000

    use by lib

--- go-selfupdate-example.go
+++ go-selfupdate-example.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/sanbornm/go-selfupdate/selfupdate"
+	"github.com/neverlock/utility/selfupdate"
 	"log"
 	"net/http"
 )
 
 func main() {
 	// Simple static webserver:
-	version := "1.0"
+	version := "2.0"
+	updateUrl := "http://www.conf.in.th/auto-update/"
+	appName := "go-selfupdate-example"
 
-	updater := &selfupdate.Updater{
-		CurrentVersion: version,
-		ApiURL:         "http://www.conf.in.th/auto-update/",
-		BinURL:         "http://www.conf.in.th/auto-update/",
-		DiffURL:        "http://www.conf.in.th/auto-update/",
-		Dir:            "update-tmp",
-		CmdName:        "go-selfupdate-example", // app name
-	}
-	if updater != nil {
-		fmt.Println("Checking for new versions...")
-		go updater.BackgroundRun()
-	}
+	go selfupdate.SelfUpdate(version, updateUrl, appName)
+
 	fmt.Println("Version:", version)
 	log.Println("Start file server at :8080")
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./"))))
